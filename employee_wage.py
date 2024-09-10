edaea33601c03c543eecb30f3b0f73a0613f3801
Pooja Konduri <poojakonduru15@gmail.com>
2024-09-10 15:50:03 +0530

    [pooja] Add:To calculate daily wage of employee

--- employee_wage.py
+++ employee_wage.py
@@ -1,15 +1,19 @@
 import random
 
 class Employee:
-    def __init__(self,name):
+    def __init__(self,name,wage_per_hour,total_hours):
         self.name = name
+        self.name = name
+        self.wage_per_hour = wage_per_hour
+        self.total_hours = total_hours
 
     def attendance_check(self):
         attendance = random.randint(0,1)
         if attendance == 1:
-            return f"{self.name} is present"
+            daily_wage = self.wage_per_hour * self.total_hours
+            return f"{self.name} is present and {daily_wage} is daily wage"
         else:
             return f"{self.name} is absent"
 
-employe1 = Employee("john")
+employe1 = Employee("john",8,20)
 print(employe1.attendance_check())
