9f67480de4944e6a52220e5c6ef6e5a1e8720e66
Pooja Konduri <poojakonduru15@gmail.com>
2024-09-10 16:53:45 +0530

    [pooja] Add:To add part time employee and wage

--- employee_wage.py
+++ employee_wage.py
@@ -1,19 +1,21 @@
 import random
 
 class Employee:
-    def __init__(self,name,wage_per_hour,total_hours):
+    def __init__(self,name,wage_per_hour,total_hours,is_part_time=False):
         self.name = name
         self.name = name
         self.wage_per_hour = wage_per_hour
         self.total_hours = total_hours
+        self.is_part_time = is_part_time
 
     def attendance_check(self):
         attendance = random.randint(0,1)
         if attendance == 1:
-            daily_wage = self.wage_per_hour * self.total_hours
+            work_hours = 8 if self.is_part_time else self.total_hours
+            daily_wage = self.wage_per_hour * work_hours
             return f"{self.name} is present and {daily_wage} is daily wage"
         else:
             return f"{self.name} is absent"
 
-employe1 = Employee("john",8,20)
-print(employe1.attendance_check())
+part_time = Employee("john",8,20,is_part_time=True)
+print(part_time.attendance_check())
