e9a3ca6627ae0f07121301a1e3c1ee0666230efa
Pooja Konduri <poojakonduru15@gmail.com>
2024-09-12 12:38:25 +0530

    [pooja] Add:check employee is present or absent

--- employee_wage.py
+++ employee_wage.py
@@ -10,6 +10,10 @@ class Employee:
             return f"{self.name} is present"
         else:
             return f"{self.name} is absent"
+        
+def main():
+    employe1 = Employee("john")
+    print(employe1.attendance_check())
 
-employe1 = Employee("john")
-print(employe1.attendance_check())
+if __name__ == "__main__":
+    main()
